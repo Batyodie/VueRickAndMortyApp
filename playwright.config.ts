0bb6e8d578c38f8252d8641015c3fd3d4945241f
snemtyrev <nemtyrev@tvil.ru>
2023-12-13 01:22:35 +0300

    fix playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,8 +70,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
-    url: baseURL,
+    command: `npx vite --port ${port}`,
+    timeout: 10 * 1000,
+    port,
     reuseExistingServer: !process.env.CI,
   },
 });
